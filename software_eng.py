a54e9d36ad92c8c43f61aec53ff09d23e1949570
jose64190 <127232553+jose64190@users.noreply.github.com>
2023-03-22 21:19:19 -0400

    decoder is finished

--- software_eng.py
+++ software_eng.py
@@ -4,8 +4,15 @@ def encoder(data):
         i = data + 3
     return data
 
+
+# Jose Nodarse did the decoder
 def decoder(data):
-    data = ''
-    for j in data:
-        j = data - 3
-    return data
+    final_data = ''
+    encoded_data = 0
+    runs = len(data)
+    for i in range(runs):
+        num = int(data[i])
+        encoded_data += num - 3
+        data += str(encoded_data)
+        encoded_data = 0
+    return final_data
